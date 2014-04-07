ccccf9e2998425677169c111e8c0724f694e9b2c
Angel Sullon <asullom@gmail.com>
2014-04-07 17:56:22 -0500

    Initial

---
+++ src/testnetbeansgit/TestNetbeansGit.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package testnetbeansgit;
+
+/**
+ *
+ * @author Asullom
+ */
+public class TestNetbeansGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // local.A
+        // local.B
+        
+    }
+    
+}
